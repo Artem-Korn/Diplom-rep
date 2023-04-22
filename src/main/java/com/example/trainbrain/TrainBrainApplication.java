7141c45eb7572322699b838739e91321b88e6cd7
Artem-Korn <Artem.Korniienko@cs.khpi.edu.ua>
2023-04-22 13:28:36 +0300

    Initial commit

---
+++ src/main/java/com/example/trainbrain/TrainBrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainBrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trainbrain/TrainBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainBrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
